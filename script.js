b2fd8e3fa4cf15487b655f8b353a71767885bd9b
Pritam Raut <pritamraut2005@gmail.com>
2025-05-09 14:30:48 +0530

    adding skill about contact resume html page

---
+++ script.js
@@ -0,0 +1,23 @@
+let opt1 = document.querySelector(".headoption1");
+
+opt1.addEventListener("click", function(event){
+    console.log("clicked");
+    window.location.href="index.html";
+    console.log(window);
+});
+
+let opt2 = document.querySelector(".headoption2");
+
+opt2.addEventListener("click", function(event){
+    console.log("clicked");
+    window.location.href="project.html";
+    console.log(window);
+});
+
+let opt3 = document.querySelector(".headoption3");
+
+opt3.addEventListener("click", function(event){
+    console.log("clicked");
+    window.location.href="project.html";
+    console.log(window);
+});
