5d80773214c5fe8e219173e1418b4efe57341570
Pritam Raut <pritamraut2005@gmail.com>
2025-05-09 16:41:39 +0530

    nav bar color change effect

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ opt1.addEventListener("click", function(event){
     console.log("clicked");
     window.location.href="index.html";
     console.log(window);
+    navTextcolor(opt1);
+
 });
 
 let opt2 = document.querySelector(".headoption2");
@@ -12,6 +14,8 @@ opt2.addEventListener("click", function(event){
     console.log("clicked");
     window.location.href="project.html";
     console.log(window);
+    navTextcolor(opt2);
+
 });
 
 let opt3 = document.querySelector(".headoption3");
@@ -20,6 +24,8 @@ opt3.addEventListener("click", function(event){
     console.log("clicked");
     window.location.href="skill.html";
     console.log(window);
+    navTextcolor(opt3);
+
 });
 
 let opt4 = document.querySelector(".headoption4");
@@ -28,6 +34,8 @@ opt4.addEventListener("click", function(event){
     console.log("clicked");
     window.location.href="about.html";
     console.log(window);
+    navTextcolor(opt4);
+
 });
 
 let opt5 = document.querySelector(".headoption5");
@@ -36,6 +44,8 @@ opt5.addEventListener("click", function(event){
     console.log("clicked");
     window.location.href="contact.html";
     console.log(window);
+    navTextcolor(opt5);
+
 });
 
 let opt6 = document.querySelector(".headoption6");
@@ -44,5 +54,9 @@ opt6.addEventListener("click", function(event){
     console.log("clicked");
     window.location.href="resume.html";
     console.log(window);
+    navTextcolor(opt6);
 });
 
+function navTextcolor(opt){
+    opt.id = "col";
+}
